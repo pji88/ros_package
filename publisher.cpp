5bf8122cccfe2290045c8a7991308c4fa99992c8
pji88 <129183565+pji88@users.noreply.github.com>
2023-04-04 15:42:56 +0900

    Update publisher.cpp

--- publisher.cpp
+++ publisher.cpp
@@ -1,23 +1,26 @@
 #include <sstream>
+#include " / " //패키지이름 / 메세지파일.h 
 
-/**
- * studied from ros tutorials
- */
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "talker"); //노드명 초기화
   ros::NodeHandle n;
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-  ros::Rate loop_rate(10);
+  
+  
+  ros::Rate loop_rate(10); //루프주기 2Hz
 
   int count = 0; // check 
   
   while (ros::ok()) // basic publisher
   {
-    std_msgs::String msg; //String msg
-    std::stringstream ss; //ss 
+    
+    std_msgs::String msg; //String 이라는 타입으로 msg라는 메시지를 선언?
+    std::stringstream ss; //ss 라는 애를 저 형식으로 선언 (대입)
     msg.data = ss.str(); 
-    ROS_INFO("%s", msg.data.c_str());
+    ROS_INFO("%s", msg.data.c_str()); //print
+    
+    
     chatter_pub.publish(msg);
 
     ros::spinOnce();
@@ -28,3 +31,6 @@ int main(int argc, char **argv)
 
   return 0;
 }
+
+
+//모두 작성된 후에는 한글을 지울것, 한글때문에 오류발생할 수 있음
